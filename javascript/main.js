3b88712fbbd511861491dec7a4bb36872ff52fbd
Osmar De Leon <osmar.oficial.619@gmail.com>
2021-09-02 23:31:25 -0600

    Terminado temporizador, terminar juego

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,14 @@ const d = document, $btnElegirUsuario = d.getElementById("elegirUsuario"),
     $contLogin = d.getElementById("login"),$contUsuarios = d.getElementById("usuarios"),
     $btnCrearNuevoUsuario = d.getElementById("crearNuevoUsuario"),$inputUsuario = d.getElementById("inputUsuario"),
     $txtError = d.getElementById("error"),$btnCerrarSesion = d.getElementById("cerrarSesion"),
-    $contRegistro = d.getElementById("registro"),$contJuego = d.getElementById("juego");
+    $contRegistro = d.getElementById("registro"),$contJuego = d.getElementById("juego1"),
+    $txtUsuario = d.getElementById("txtUsuario"),$txtMaximaPuntuacion = d.getElementById("txtMaximaPuntuacion"),
+    $txtNivel = d.getElementById("txtNivel"),$txtXP = d.getElementById("txtXP"),$contJuego2 = d.getElementById("juego2"),
+    $txtModo = d.getElementById("txtModo"),$txtTiempo = d.getElementById("txtTiempo"),$txtVidas = d.getElementById("txtVidas"),
+    $btnTerminarJuego = d.getElementById("terminarJuego");
+
+
+let usuario,modoJuego,tiempo,vidas=3,contador;
 
 // FUNCIONES
 const EsconderContRegistro = () => {
@@ -32,9 +39,12 @@ const ComprobarSiExisteUsuario = () => {
         $txtError.textContent = "Este usuario ya Existe"
     }else{
         $txtError.textContent = "";
+        usuario = $inputUsuario.value;
         CrearUsuario();
         MostrarUsuarios();
         $contLogin.reset();
+        $contRegistro.classList.add("esconder");
+        EntrarJuego();
     }
 }
 
@@ -46,6 +56,48 @@ const CrearUsuario = () => {
     }));
 }
 
+const EntrarJuego = () => {
+    $contJuego.classList.remove("esconder");
+    const DatosJugador = JSON.parse(localStorage.getItem(usuario));
+    $txtUsuario.textContent = usuario;
+    $txtMaximaPuntuacion.textContent = DatosJugador.scoreMax;
+    $txtNivel.textContent = DatosJugador.lvl;
+    $txtXP.textContent = `${DatosJugador.xp} / 100`; 
+}
+
+const IniciarJuego = () => {
+    $contJuego.classList.add("esconder");
+    $contJuego2.classList.remove("esconder");
+    switch(modoJuego){
+        case 'facil':
+            $txtModo.textContent = "Facíl";
+            tiempo = 30;
+            break;
+        case 'intermedio':
+            $txtModo.textContent = "Intermedio";
+            tiempo = 20;
+            break;
+        default:
+            $txtModo.textContent = "Difícil";
+            tiempo = 15;
+            break;
+    }
+    $txtTiempo.textContent = tiempo;
+    $txtVidas.textContent = vidas;
+    contador = setInterval(() => {
+        tiempo--;
+        $txtTiempo.textContent = tiempo;
+        if(tiempo === 0) Resetear();
+    },1000);
+}
+
+const Resetear = () =>{
+    tiempo = 0;
+    vidas = 3;
+    modoJuego = "";
+    clearInterval(contador);
+}
+
 // EVENTOS
 window.addEventListener("load",() =>    MostrarUsuarios());
 
@@ -53,11 +105,31 @@ $btnElegirUsuario.addEventListener("click",() => EsconderContRegistro());
 
 $btnCrearNuevoUsuario.addEventListener("click",() => EsconderContRegistro());
 
-$contLogin.addEventListener("submit",() => {
+$contLogin.addEventListener("submit",(e) => {
+    e.preventDefault();
     ($inputUsuario.value === "") ? $txtError.textContent = "Ingresar Usuario" : ComprobarSiExisteUsuario();
 });
 
 $btnCerrarSesion.addEventListener("click",() => {
     $contJuego.classList.toggle("esconder");
     $contRegistro.classList.toggle("esconder");
+});
+
+d.addEventListener("click",(e) => {
+    if(e.target.matches("#contenedorBtnUsuarios button")){
+        $contRegistro.classList.add("esconder");
+        usuario = e.target.textContent;
+        EntrarJuego();
+    }
+
+    if(e.target.matches("#escogerModoJuego button")){
+        modoJuego = e.target.value;
+        IniciarJuego();
+    }
+});
+
+$btnTerminarJuego.addEventListener("click", () => {
+    Resetear();
+    $contJuego2.classList.add("esconder");
+    $contJuego.classList.remove("esconder");
 });
